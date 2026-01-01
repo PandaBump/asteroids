7da75618d019e79b556605409be3574875e4fe89
PandaBump <stokesk.swe@gmail.com>
2026-01-01 17:20:43 -0500

    add: asteroids and asteroid field to spawn asteroids

---
+++ asteroid.py
@@ -0,0 +1,17 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, LINE_WIDTH
+from logger import log_state
+from player import Player
+from circleshape import CircleShape
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, 'white', self.position, self.radius, LINE_WIDTH)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
+
+
