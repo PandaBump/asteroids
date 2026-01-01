f3cc1e9cffa8ae7ff7f425adb3983aab2587a92a
PandaBump <stokesk.swe@gmail.com>
2026-01-01 16:05:00 -0500

    add: player ship visual spawn and mobility

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from constants import * #or SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
-
+from player import *
 
 
 def main():
@@ -10,16 +10,23 @@ def main():
     VERSION = pygame.version.ver
     print(f"Starting Asteroids with pygame version: {VERSION}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) # Create a clock object to manage frame rate
+    dt = 0 # Delta time between frames
 
     while 1 > 0:
         log_state()
-        for event in pygame.event.get():
+
+        for event in pygame.event.get(): # Event handling for quitting the game
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, color='black')
+
+        screen.fill('black')
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
 
-
+        dt = clock.tick(60) / 1000.0
 
 
 if __name__ == "__main__":
