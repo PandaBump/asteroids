3c3e2d93a4f60504d202c85c6ee4e6e4e3604d66
PandaBump <stokesk.swe@gmail.com>
2026-01-01 18:54:28 -0500

    add: shot collision with asteroids and splitting for asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,11 +1,14 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH, LINE_WIDTH
-from logger import log_state
+import random
+from constants import *
 from player import Player
 from circleshape import CircleShape
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
+        asteroid1 = None
+        asteroid2 = None
         super().__init__(x, y, radius)
 
     def draw(self, screen):
@@ -14,4 +17,17 @@ class Asteroid(CircleShape):
     def update(self, dt):
         self.position += self.velocity * dt
 
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            log_event("asteroid_split")
+            random_angle = random.uniform(20, 50)
+            asteroid1 = Asteroid(self.position.x, self.position.y, self.radius - ASTEROID_MIN_RADIUS)
+            asteroid2 = Asteroid(self.position.x, self.position.y, self.radius - ASTEROID_MIN_RADIUS)
+            asteroid1.velocity = self.velocity.rotate(random_angle) * 1.2
+            asteroid2.velocity = self.velocity.rotate(-random_angle) * 1.2
+            return [asteroid1, asteroid2]
 
--- main.py
+++ main.py
@@ -42,6 +42,13 @@ def main():
                 log_event("player_hit")
                 print("Player hit an asteroid!\nGame over!")
                 sys.exit()
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    asteroid.split()
+                    shot.kill()
+                    print("Asteroid hit!")
         pygame.display.flip()
 
         # Limit the frame rate to 60 FPS and calculate delta time
